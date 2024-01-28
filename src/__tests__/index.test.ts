2624b7c7a4636eebb3789c6b048abaa2609a5c7f
Rohit Agrawal <rohitagrawalofficialmail@gmail.com>
2024-01-28 13:34:48 -0800

    test: updated unit tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,7 +1,12 @@
 import { expect } from "@jest/globals";
+import data from "../index";
 
 describe("Template Test", () => {
   it("should pass", () => {
     expect(true).toBe(true);
+  });
+
+  it("should return empty object", () => {
+    expect(data).toEqual({});
   });
 });
